1913bb2ae5e9793b2b60d9f2275a5d11f07ef406
1095272620@qq.com <19300405ymk>
2021-10-03 14:01:52 +0800

    init commit

---
+++ code/download_xapk.py
@@ -0,0 +1,79 @@
+from selenium import webdriver
+import os
+import shutil
+import re
+import time
+import sqlite3
+
+options = webdriver.ChromeOptions()
+prefs = {
+    'download.default_directory': "F:\\temp",
+    'safebrowsing.enabled': 'false',
+}
+options.add_experimental_option('prefs', prefs)
+driver = webdriver.Chrome(options=options)
+
+
+def get_download_dir():
+    return "F:\\temp"
+
+
+def get_stored_dir():
+    return "F:\\download"
+
+
+def download_apk(url):
+    if url != "":
+        driver.get(url)
+        file_name = re.sub(r" \(\d+\.?\d* MB\)", "", driver.find_element_by_class_name("file").text)
+
+        while not check_finished(file_name):
+            print("downloading %s" % file_name)
+            time.sleep(3)
+        print("{+} download %s success" % file_name)
+        move_apk(file_name)
+    else:
+        print("{+} no valid url provided")
+
+
+def check_finished(apk_name):
+    download_dir = get_download_dir()
+    apk_file_path = os.path.join(download_dir, apk_name)
+    if os.path.isfile(apk_file_path):
+        return True
+    return False
+
+
+def move_apk(apk_name):
+    download_dir = get_download_dir()
+    download_path = os.path.join(download_dir, apk_name)
+    stored_dir = get_stored_dir()
+
+    if os.path.isdir(stored_dir) and os.path.isfile(download_path):
+        if os.path.exists(os.path.join(stored_dir, apk_name)):
+            os.remove(os.path.join(stored_dir, apk_name))
+        shutil.move(download_path, stored_dir)
+
+
+if __name__ == "__main__":
+    start_index = 45
+    con = sqlite3.connect("../../apk_pure.db")
+    cur = con.cursor()
+    task_count_res = cur.execute("select count(*) from apk_info")
+    task_count = task_count_res.fetchone()
+    while start_index < 45 + task_count[0]:
+        sql = '''SELECT * FROM apk_info WHERE id = ?'''
+        cur.execute(sql, (start_index,))
+        download_link_res = cur.fetchall()
+        if len(download_link_res) == 0:
+            print("{+} no download link find")
+        else:
+            download_apk(download_link_res[0][2])
+            time.sleep(2)
+
+        # 更新表长度
+        task_count_res = cur.execute("select count(*) from apk_info")
+        task_count = task_count_res.fetchone()
+        print(task_count[0])
+        start_index = start_index + 1
+        print("----------------------------")
---
+++ code/get_xapk_download_link.py
@@ -0,0 +1,113 @@
+import re
+import time
+from queue import Queue
+
+from sqlite3 import IntegrityError
+import sqlite3
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+
+options = webdriver.ChromeOptions()
+prefs = {
+    'download.default_directory': "E:\\temp",
+    'safebrowsing.enabled': 'false'
+}
+options.add_experimental_option('prefs', prefs)
+driver = webdriver.Chrome(options=options)
+
+# 用于记录已经访问过的app
+requested_app_id = set()
+# 用于记录需要下载的任务
+download_task = Queue()
+
+con = sqlite3.connect("../../apk_pure.db")
+
+
+def get_download_link(app_id):
+    if app_id in requested_app_id:
+        print("{+} %s has request before" % app_id)
+        return
+    else:
+        requested_app_id.add(app_id)
+
+    driver.get("https://apkpure.com/search?q=%s" % app_id)
+    download_link = ""
+    success = False
+
+    try:
+        apk_detail_url = driver.find_element_by_tag_name("p>a").get_attribute('href')
+        # 将similar apk加入任务队列中
+        add_similar_app_id_to_mission(apk_detail_url)
+        driver.get(apk_detail_url + "/versions")
+    except NoSuchElementException:
+        print("{+} no result find in apk pure ")
+        return ""
+    if "Page Deleted or Gone" in driver.title:
+        print("{+} fail to get download link, page maybe deleted or gone")
+        return ""
+
+    for apk_details in driver.find_elements_by_class_name("ver-item"):
+        if 'XAPK' in apk_details.text and 'OBB' not in apk_details.text:
+            try:
+                variants = apk_details.find_element_by_class_name("ver-item-v")
+                if 'Variants' in variants.text:
+                    driver.get(apk_detail_url + "/variant/" +
+                               apk_details.find_element_by_class_name("ver-item-n").text[1:] + "-XAPK")
+            except NoSuchElementException:
+                download_link = driver.find_element_by_css_selector(".ver-wrap>li>a").get_attribute("href")
+                success = True
+                break
+            try:
+                download_link = driver.find_element_by_css_selector(".table-cell>a").get_attribute("href")
+                success = True
+                break
+            except NoSuchElementException:
+                download_link = ""
+                success = False
+
+    if success:
+        print("{+} find download link %s" % download_link)
+        add_app_info_to_db(app_id, download_link)
+    else:
+        print("{+} no apks find, fail to find download link")
+
+    return download_link
+
+
+def add_similar_app_id_to_mission(apk_detail_url):
+    driver.get(apk_detail_url)
+    top_list = driver.find_element_by_css_selector(".top-list")
+    for _ in top_list.find_elements_by_css_selector("a[target=\"_blank\"]"):
+        similar_app_id = _.get_attribute('href')
+        if re.match(r"https://apkpure.com/[\s\S]*/[\s\S]*", similar_app_id):
+            similar_app_id = similar_app_id.split('/')[-1]
+            if similar_app_id not in requested_app_id:
+                download_task.put(similar_app_id)
+
+
+def add_app_info_to_db(app_id, download_link):
+
+    cur = con.cursor()
+    sql = '''INSERT INTO apk_info(app_id, download_link) VALUES (?, ?)'''
+    data = (app_id, download_link)
+
+    try:
+        cur.execute(sql, data)
+    except IntegrityError:
+        print("{+} app_id duplicate")
+    con.commit()
+
+
+if __name__ == "__main__":
+    with open("../base-app", "r") as f:
+        for line in f.readlines():
+            app_id = line.replace("\n", "")
+            download_task.put(app_id)
+
+    while download_task.qsize() != 0:
+        time.sleep(1)
+        app_id = download_task.get()
+        print("{+} start find download link %s" % app_id)
+        get_download_link(app_id)
+        print(str(download_task.qsize()) + " tasks remaining")
+        print("--------------------------")
