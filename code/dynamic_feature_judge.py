3ac95235e6ebc8f95ae12b837945355b091c29e0
1095272620@qq.com <19300405ymk>
2021-10-04 16:30:10 +0800

    improve code

--- code/dynamic_feature_judge.py
+++ code/dynamic_feature_judge.py
@@ -6,15 +6,16 @@ def read_file_in_xapk(file_path):
     target_file = []
     with zipfile.ZipFile(file_path, 'r') as xapk_file:
         for apk_file in xapk_file.namelist():
-            if apk_file.endswith("apk") and not apk_file.startswith("config"):
+            if apk_file.endswith("apk") and "config" not in apk_file:
                 target_file.append(apk_file)
     if len(target_file) > 1:
+        print(target_file)
         return True
     return False
 
 
 if __name__ == "__main__":
-    download_path = "E:\\download"
+    download_path = "F:\\download"
 
     for xapk_file in os.listdir(download_path):
         if not xapk_file.endswith("xapk"):
@@ -22,3 +23,4 @@ if __name__ == "__main__":
         xapk_file_path = os.path.join(download_path, xapk_file)
         if read_file_in_xapk(xapk_file_path):
             print(xapk_file_path)
+            print("------------------------")
